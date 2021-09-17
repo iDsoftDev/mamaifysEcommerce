ace9737a6b94a0a1c918be6dbc2e388dc2f2bc35
IdamEniIdam <idameni89@gmail.com>
2021-09-17 03:53:36 -0400

    Using dot env files

--- controller/auth.js
+++ controller/auth.js
@@ -151,7 +151,7 @@ const forgotPassword = asyncHandler(async (req, res, next) => {
   await user.save({ validateBeforeSave: false });
 
   try {
-    const resetUrl = `http://192.168.43.154:3000/reset-password/?token=${resetToken}`;
+    const resetUrl = `https://mamaify.herokuapp.com/reset-password/?token=${resetToken}`;
 
     const message = `You are receiving this email because you (or someone else ) has
     requested the reset of a password.`;
