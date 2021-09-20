3294a7396b95814d495e617435b5b2d13e5dfc38
IdamEniIdam <idameni89@gmail.com>
2021-09-20 10:34:25 -0400

    de configured

---
+++ client/build/precache-manifest.109ec2db9aec8f6646dd8a0aac1c4942.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0c02d1bf3515fef15dc46a7e5ed1716",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2986b035e1ad79f6f2eb",
+    "url": "/static/css/2.bb803c14.chunk.css"
+  },
+  {
+    "revision": "3362f8e08bcc1483dc54",
+    "url": "/static/css/main.80be173e.chunk.css"
+  },
+  {
+    "revision": "2986b035e1ad79f6f2eb",
+    "url": "/static/js/2.e8c306cf.chunk.js"
+  },
+  {
+    "revision": "3d52bd4e26e2416c70b88b5aaa0fe8b4",
+    "url": "/static/js/2.e8c306cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3362f8e08bcc1483dc54",
+    "url": "/static/js/main.2b1171ae.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "31a737c539bf89313cd1b4d5f43883fd",
+    "url": "/static/media/confirmation.31a737c5.png"
+  }
+]);
